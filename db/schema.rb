5456da5e8fbc446e2eab8ea969f153832e3669f9
Marcle Rodrigues <maarclee@gmail.com>
2019-03-19 22:18:39 -0300

    Add access token to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_20_000353) do
+ActiveRecord::Schema.define(version: 2019_03_20_002804) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2019_03_20_000353) do
     t.bigint "balance"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "access_token"
   end
 
 end
